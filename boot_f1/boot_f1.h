f3fe3b9c918e0175cdffabc600ed22be1930ebb6
vthanhkhoa <vothanhkhoaaa@gmail.com>
2024-03-28 10:58:58 +0100

    fix: read&write func

--- boot_f1/boot_f1.h
+++ boot_f1/boot_f1.h
@@ -1,8 +1,11 @@
 #ifndef __BOOT_F1__
 #define __BOOT_F1__
 
+#include "main.h"
+
 void flash_erase(volatile uint32_t addreg);
 void flash_write(volatile uint32_t addreg, uint8_t *datawrite, uint32_t datalen);
 void flash_read(volatile uint32_t addreg, uint8_t *dataread, uint32_t datalen);
 void bootloader(volatile uint32_t addr_new);
+void boot_to_app(void);
 #endif
