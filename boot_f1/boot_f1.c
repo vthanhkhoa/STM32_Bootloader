aa3134d2f7c8a6134eae659d7f6aaacef9d80676
vthanhkhoa <vothanhkhoa@gmail.com>
2024-04-10 16:26:54 +0700

    fix: update boot2() function

--- boot_f1/boot_f1.c
+++ boot_f1/boot_f1.c
@@ -1,7 +1,5 @@
 #include "boot_f1.h"
 
-#define app_base_addr 0x08010000    
-
 void flash_unlock()
 {
     FLASH->KEYR = 0x45670123;
@@ -58,16 +56,45 @@ void bootloader(volatile uint32_t addr_new)
     NVIC_SystemReset();
 }
 
+/**
+ * The function `boot_to_app` sets the main stack pointer and calls the reset handler function to boot
+ * into an application.
+ */
 void boot_to_app(void)
 {
     void (*app_reset_handler)(void) = NULL;
     uint32_t app_main_stack_pointer = 0;
     uint32_t app_reset_handler_addr = 0;
 
-    app_main_stack_pointer = *(volatile uint32_t *)(app_base_addr);
+    app_main_stack_pointer = *(volatile uint32_t *)(BASE_ADDRESS_FLASH);
     __set_MSP(app_main_stack_pointer);
 
-    app_reset_handler_addr = *(volatile uint32_t *)(app_base_addr + 0x4U);
+    app_reset_handler_addr = *(volatile uint32_t *)(BASE_ADDRESS_FLASH + 0x4U);
     app_reset_handler = (void *)app_reset_handler_addr;
     app_reset_handler();
+}
+
+/**
+ * The `boot2` function resets the clock, initializes the application reset handler, and jumps to the
+ * application code.
+ */
+void boot2(void)
+{
+    void (*app_reset_handler)(void) = (void *)(*((volatile uint32_t *)(BASE_ADDRESS_FLASH + 4U)));
+
+    // Turn OFF the Green Led to tell the user that Bootloader is not running
+    //	  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0, GPIO_PIN_RESET );    //Green LED OFF
+
+    /* Reset the Clock */
+    HAL_RCC_DeInit();
+    HAL_DeInit();
+    __set_MSP(*(volatile uint32_t *)BASE_ADDRESS_FLASH);
+    SysTick->CTRL = 0;
+    SysTick->LOAD = 0;
+    SysTick->VAL = 0;
+
+    /* Jump to application */
+    //	  SCB->VTOR = BASE_ADDRESS_FLASH;
+    app_reset_handler(); // call the app reset handler
+    //	  NVIC_SystemReset();
 }
